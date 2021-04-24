8bb9f85121d9a85330e00e017fe2558e4b0a0b12
Yarviz <juzzzzzi@hotmail.com>
2021-04-24 12:32:16 +0300

    update shell script

--- runserver.sh
+++ runserver.sh
@@ -4,7 +4,11 @@ if [ $# -lt 2 ]; then
 	exit 1
 fi
 echo "Starting nucleo server..."
-./indoorair-server "$2" > /dev/null 2>&1 &
+if [ -z "$3" ]; then
+	./indoorair-server "$2" > /dev/null 2>&1 &
+else
+	sudo ./indoorair-server "$2" "$3" &
+fi
 echo "Starting webserver..."
 echo
 python3 indoorair.py "$1"
